7b5d82377f238be86c42941dbe331db35b72e685
Johnny Pestana <johnnygpestana@gmail.com>
2025-07-17 20:13:34 -0300

    general adjusts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { PerfRunnerComponent } from './perf-runner/perf-runner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PerfRunnerComponent],
+  imports: [PerfRunnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
